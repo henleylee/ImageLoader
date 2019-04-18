88920f7a9c1baf49ae9639c836068e55453f358c
李云龙 <liyunlong@caiyikeji.com>
2019-04-18 18:06:06 +0800

    Upload apk

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ImageLoader —— Android 图片加载框架封装(策略模式)
+
+## 特点 ##
+ - 低耦合，方便将来的代码扩展；
+ - 满足项目中各种需求，调用方便；
+ - 支持目前市场上使用率最高的图片框架 Glide、Fresco、Picasso 之间的切换
+
+## APK Demo ##
+
+下载 [APK-Demo](https://github.com/HenleyLee/ImageLoader/raw/master/app/app-release.apk)
+
+
+
+
--- app/src/main/java/com/henley/imageloader/sample/SampleGlideModule.java
+++ app/src/main/java/com/henley/imageloader/sample/SampleGlideModule.java
@@ -1,11 +1,14 @@
 package com.henley.imageloader.sample;
 
+import com.bumptech.glide.annotation.GlideModule;
 import com.henley.imageloader.glide.ImageGlideModule;
 
 /**
- * @author liyunlong
+ * Glide配置
+ *
+ * @author Henley
  * @date 2019/4/18 17:28
  */
-//@GlideModule
+@GlideModule
 public class SampleGlideModule extends ImageGlideModule {
 }
